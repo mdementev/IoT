675efacf6028a42026744a09dce1eafea9219908
Dmitry <dark.knight.9111@gmail.com>
2015-11-14 18:56:30 +0300

    Настройки

--- IoT/Scripts/Common/Common.js
+++ IoT/Scripts/Common/Common.js
@@ -1,34 +1,11 @@
-﻿var app = angular.module('StarterApp', ['ngMaterial']);
+﻿window.app = angular.module('StarterApp', ['ngMaterial']);
 
-window.app = angular.module('StarterApp', ['ngMaterial']);
-app.controller('AppController', function () {
 
+app.controller('AppController', function ($scope) {
 });
 
-
-    angular.module('fabToolbarBasicUsageDemo', ['ngMaterial'])
-      .controller('AppController', function ($scope) {
-          $scope.isOpen = false;
-          $scope.demo = {
-              isOpen: false,
-              count: 0,
-              selectedDirection: 'left'
-          };
-      });
-
-
-    angular.module('inputErrorsApp', ['ngMaterial', 'ngMessages'])
-    .controller('AppController', function ($scope) {
-        $scope.project = {
-            description: 'Nuclear Missile Defense System',
-            rate: 500
-        };
-    });
-
-
-
-
 populateAccessToken();
+
 function populateAccessToken() {
     $.ajaxSetup({
         headers: {
